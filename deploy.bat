0ff3849117ecfb26bf1eb7e128512ee4bbee78ef
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2022-04-13 21:31:48 +0800

    Wed 13/04/2022

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,4 @@
+:: the code needs to be built to docs/ and thats what the app looks to to run
 @REM build the app
 @REM builder.bat
 
@@ -6,10 +7,5 @@ git pull
 git add --all
 git commit -m "%date%"
 git push
-
-@REM @REM push the code to the branch that the site runs off
-@REM git add dist && git commit -m "push to website"
-@REM git subtree push --prefix dist origin gh-pages
-@REM git subtree pull --prefix dist origin gh-pages
 
 
