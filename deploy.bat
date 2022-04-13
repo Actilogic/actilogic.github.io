ead1040c21783c3f76020d81ba514e8634c51b3b
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2022-04-13 19:50:53 +0800

    Wed 13/04/2022

--- deploy.bat
+++ deploy.bat
@@ -4,9 +4,14 @@ echo MAKE SURE THAT YOU BUILT THIS BEFORE YOU PUSH!!!!!!! (ng build)
 @REM REM ng build 
 @echo on
 
-
+@REM back up code to master which will then be up to date with prod
 git pull
 git add --all
 git commit -m "%date%"
 git push
-ng build 
+
+@REM push the code to the branch that the site runs off
+git add dist && git commit -m "push to website"
+git subtree push --prefix dist origin gh-pages
+
+
