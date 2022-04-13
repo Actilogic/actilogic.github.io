df2710560d230f57d09d4cbf37e355856006e902
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2022-04-13 21:23:34 +0800

    Wed 13/04/2022

--- deploy.bat
+++ deploy.bat
@@ -7,9 +7,9 @@ git add --all
 git commit -m "%date%"
 git push
 
-@REM push the code to the branch that the site runs off
-git add dist && git commit -m "push to website"
-git subtree push --prefix dist origin gh-pages
-git subtree pull --prefix dist origin gh-pages
+@REM @REM push the code to the branch that the site runs off
+@REM git add dist && git commit -m "push to website"
+@REM git subtree push --prefix dist origin gh-pages
+@REM git subtree pull --prefix dist origin gh-pages
 
 
