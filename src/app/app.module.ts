e4047855a9ceb7695ee4f6489d63b950a1687c60
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2022-04-13 19:13:15 +0800

    updated structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NavigationBarComponent } from '../components/navigation-bar/navigation-bar.component';
+import { HomeComponent } from '../components/home/home.component';
+import { FuelSaverComponent } from '../components/fuel-saver/fuel-saver.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from '../components/footer/footer.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationBarComponent,
+    HomeComponent,
+    FuelSaverComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
