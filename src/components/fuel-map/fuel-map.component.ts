69eacda93bdd11d5cfd42fa37fd32927375c5143
antoniojoboy <63899957+antoniojoboy@users.noreply.github.com>
2022-04-20 13:01:00 +0800

    e

--- src/components/fuel-map/fuel-map.component.ts
+++ src/components/fuel-map/fuel-map.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
 import { MapService } from 'src/services/map/map.service';
 
 @Component({
@@ -13,7 +12,29 @@ export class FuelMapComponent implements OnInit {
   constructor(private map: MapService) { }
 
   ngOnInit() {
-    this.map.buildMap()
+
+
+    const geojson = {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            // coordinates: [-32.021389, 115.875188]
+            coordinates: [115.875188, -32.021389]
+          },
+          properties: {
+            title: 'Mapbox',
+            description: 'Washington, D.C.'
+          }
+        }
+      ]
+    };
+
+    this.map.buildMap();
+    this.map.addMarkers(geojson);
+
   }
 
 }
--- src/services/map/map.service.ts
+++ src/services/map/map.service.ts
@@ -2,18 +2,29 @@ import { Injectable } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from "../../environments/environment";
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
   map: mapboxgl.Map;
-  style = 'mapbox://styles/mapbox/streets-v11';
-  lat = 45.899977;
-  lng = 6.172652;
+  style = 'mapbox://styles/mapbox/navigation-night-v1';
+  lat = -32.021389;
+  lng = 115.875188;
   zoom = 12
 
   constructor() {
     mapboxgl.accessToken = environment.mapbox.accessToken;
+  }
+
+  addMarkers(geojson) {
+    for (const feature of geojson.features) {
+      // create a HTML element for each feature
+      const el = document.createElement('div');
+      el.className = 'marker';
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(this.map);
+    }
   }
 
   buildMap() {
